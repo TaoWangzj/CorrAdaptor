0a5fdb95c14825f28ff609ae2d0a5116a37f5dd8
Tao Wang <taowangzj@gmail.com>
2024-08-15 15:56:54 +0800

    Update test.py

--- test.py
+++ test.py
@@ -27,18 +27,23 @@ if __name__ == '__main__':
     param_count = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print("Model has {} parameters".format(param_count))
 
-    # for i in [14, 15]:
-    #     weights_dict = torch.load(f"/data/sets/liuyicheng/codes/corrformer/checkpoint/corrformer_v5_3_node_250/checkpoint{i}.pth", map_location="cuda")
-    #     safe_load_weights(model, weights_dict['state_dict'])
+    # download pre-trained models from links in dir weights
+    # for fycc
+    weights_dict = torch.load(os.path.join('./weights', 'yfcc_best.pth'), map_location="cuda")
 
-    #     aucs5, aucs10, aucs20, va_res, precisions, recalls, f_scores = evaluate(model, test_loader, conf, epoch=i)
-    #     va = [aucs5, aucs10, aucs20, va_res[0] * 100, va_res[1] * 100, va_res[2] * 100, va_res[3] * 100, precisions * 100, recalls * 100, f_scores * 100]
+    # for sun3d
+    # weights_dict = torch.load(os.path.join('./weights', 'sun3d_best.pth'), map_location="cuda")
+    
+    safe_load_weights(model, weights_dict['state_dict'])
 
-    #     output = ''
-    #     name = ["AUC@5", "AUC@10", "AUC@20", "mAP5", "mAP10", "mAP15", "mAP20", "Precisions", "Recalls", "F_scores"]
-    #     for i, j in enumerate(va):
-    #         output += name[i] + ": " + str(j) + "\n"
+    aucs5, aucs10, aucs20, va_res, precisions, recalls, f_scores = evaluate(model, test_loader, conf, epoch=i)
+    va = [aucs5, aucs10, aucs20, va_res[0] * 100, va_res[1] * 100, va_res[2] * 100, va_res[3] * 100, precisions * 100, recalls * 100, f_scores * 100]
 
-    #     print(output)
+    output = ''
+    name = ["AUC@5", "AUC@10", "AUC@20", "mAP5", "mAP10", "mAP15", "mAP20", "Precisions", "Recalls", "F_scores"]
+    for i, j in enumerate(va):
+        output += name[i] + ": " + str(j) + "\n"
+
+    print(output)
 
 # CUDA_VISIBLE_DEVICES=5 python test.py
